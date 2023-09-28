c99f73b01ef831d30b7415f514031f0dd66628ae
adrianMartinezTeba <adrymate@gmail.com>
2023-09-28 18:35:16 +0200

    mejorando estructura general del proycto

--- src/modelosGlb.js
+++ src/modelosGlb.js
@@ -2,14 +2,14 @@ const modelos = [
     {
         nombre: "Mesa",
         urlCatalogo: "./Mesa/pruebaM.gltf",
-        urlInd:'./Mesa/pruebaM.gltf',
+        urlInd:'../../Mesa/pruebaM.gltf',
         escalaCat:0.1,
         escalaInd:0.05,
         descripcion: "Mesa de trabajo"
     },{
         nombre: 'Coche',
         urlCatalogo: './Coche/coche.gltf',
-        urlInd:'./Coche/coche.gltf',
+        urlInd:'../../Coche/coche.gltf',
         escalaCat: 0.2,
         escalaInd:0.3,
         descripcion: "Coche de carreras"
